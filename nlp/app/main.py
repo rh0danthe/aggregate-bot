450754fac22a4413c3d1c8b24353d5a521ebb246
Shnayder Kirill <Knhn0@yandex.ru>
2024-03-23 03:30:27 +0300

    fix: cleanup

--- nlp/app/main.py
+++ nlp/app/main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get("/{lost_json}")
 async def read_root(lost_json: str):
     return {"lost": nlp.lost(lost_json)}
+
+
+@app.get("/")
+async def ping():
+    return {"pong":":)"}
