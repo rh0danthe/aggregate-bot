34c9285019a9f8e0c69acd18d7bc3dc7e6c1eb11
rh0danthe <rita.pichushkina.04@mail.ru>
2024-03-23 18:06:57 +0300

    dto changed

---
+++ backend/backend/Dto/Msg.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace backend.Dto;
+
+public class Response
+{
+    [JsonPropertyName("title")]
+    public string Title { get; set; }
+    
+    [JsonPropertyName("chat_id")]
+    public long ChatId { get; set; }
+    
+    [JsonPropertyName("msg_id")]
+    public long MessageId { get; set; }
+    
+    [JsonPropertyName("session_string")]
+    public string SessionString { get; set; }
+    
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+}
---
+++ backend/backend/Dto/Response.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace backend.Dto;
+
+public class Request
+{
+    [JsonPropertyName("msgs")]
+    public ICollection<Msg> Messages { get; set; }
+}
