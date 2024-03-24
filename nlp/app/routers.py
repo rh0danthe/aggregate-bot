0739f4e299fc66819f020a63936eea4603fda300
username <voditel.vlad@yandex.ru>
2024-03-24 18:21:17 +0300

    add request to api

--- nlp/app/routers.py
+++ nlp/app/routers.py
@@ -3,15 +3,16 @@ from fastapi import APIRouter
 from nlp_unit import form_title
 from entities import MsgsFromBot
 from tokenization import check_accord, check_lost_found
+import requests
 
 #@app.get("/backend/approved")
 
 router = APIRouter()
-@router.get("/backend/from_bot")
+@router.post("/backend/from_bot")
 async def from_bot(msgs_from_bot: MsgsFromBot): #collection
     #сортировка по is_found
     accorded = check_accord(msgs_from_bot)
     lost_found = check_lost_found(accorded)
     formed = form_title(lost_found)
-
+    r = requests.post('http://api:8080/backend/approved', json=formed)
     return formed
