fb18c31a77dd7c5e3ef793c8f0126963bc4e6c3d
username <voditel.vlad@yandex.ru>
2024-03-24 18:52:31 +0300

    changed imports

--- nlp/app/routers.py
+++ nlp/app/routers.py
@@ -1,18 +1,18 @@
 #from main import app
 from fastapi import APIRouter
-from nlp_unit import form_title
-from entities import MsgsFromBot
-from tokenization import check_accord, check_lost_found
+from . import entities #from entities import MsgsFromBot
+from . import tokenization #import check_accord, check_lost_found
+from . import nlp_unit
 import requests
 
 #@app.get("/backend/approved")
 
 router = APIRouter()
 @router.post("/backend/from_bot")
-async def from_bot(msgs_from_bot: MsgsFromBot): #collection
+async def from_bot(msgs_from_bot: entities.MsgsFromBot): #collection
     #сортировка по is_found
-    accorded = check_accord(msgs_from_bot)
-    lost_found = check_lost_found(accorded)
-    formed = form_title(lost_found)
+    accorded = tokenization.check_accord(msgs_from_bot)
+    lost_found = tokenization.check_lost_found(accorded)
+    formed = nlp_unit.form_title(lost_found)
     r = requests.post('http://api:8080/backend/approved', json=formed)
     return formed
