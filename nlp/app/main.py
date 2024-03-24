c899046f6ba91de12607bc4aa36a5a9ad3de85b1
username <voditel.vlad@yandex.ru>
2024-03-24 19:48:56 +0300

    imports troubleshooting

--- nlp/app/main.py
+++ nlp/app/main.py
@@ -1,6 +1,7 @@
 from typing import Union
 from fastapi import FastAPI
-from . import routers
+#from . import routers
+import routers
 
 app = FastAPI()
 app.include_router(routers.router)
--- nlp/app/routers.py
+++ nlp/app/routers.py
@@ -1,18 +1,18 @@
 #from main import app
 from fastapi import APIRouter
-from . import entities #from entities import MsgsFromBot
-from . import tokenization #import check_accord, check_lost_found
-from . import nlp_unit
+from nlp_unit import form_title
+from entities import MsgsFromBot
+from tokenization import check_accord, check_lost_found
 import requests
 
 #@app.get("/backend/approved")
 
 router = APIRouter()
-@router.post("/backend/from_bot", response_model=entities.MsgsFromBot)
-async def from_bot(msgs_from_bot: entities.MsgsFromBot): #collection
+@router.post("/backend/from_bot")
+async def from_bot(msgs_from_bot: MsgsFromBot): #collection
     #сортировка по is_found
-    accorded = tokenization.check_accord(msgs_from_bot)
-    lost_found = tokenization.check_lost_found(accorded)
-    formed = nlp_unit.form_title(lost_found)
-    r = requests.post('http://api:8080/backend/approved', json=formed)
+    accorded = check_accord(msgs_from_bot)
+    lost_found = check_lost_found(accorded)
+    formed = form_title(lost_found)
+    #requests.post('http://api:8080/backend/approved')
     return formed
