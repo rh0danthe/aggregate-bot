7ac33df510705718c1fe1d11ca323cf55530e19f
rh0danthe <rita.pichushkina.04@mail.ru>
2024-03-25 03:57:20 +0300

    anticomment

--- backend/backend/Controllers/ApprovedMessagesController.cs
+++ backend/backend/Controllers/ApprovedMessagesController.cs
@@ -21,15 +21,11 @@ public class ApprovedMessagesController : Controller
     [HttpPost]
     public async Task<IActionResult> Upload([FromBody] NeuralRequest request)
     {
-        Console.WriteLine("Error blyad");
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
-        //Console.WriteLine("PENIIIIIIIIIIIIIIIIIS");
-        var res = await _approvedMessagesService.CreateAsync(request.msgs, request.is_found, request.session);
-        //var res = await _approvedMessagesService.GetAllByKeywordsAsync(request.keywords, request.session);
-       // Console.WriteLine("PENIIIIIIIIIIIIIIIIIS");
+        await _approvedMessagesService.CreateAsync(request.msgs, request.is_found, request.session);
+        var res = await _approvedMessagesService.GetAllByKeywordsAsync(request.keywords, request.session);
         await _bot.PostAsync(res, request.session);
-        Console.WriteLine("We did it");
         return Ok(res);
     }
 }
