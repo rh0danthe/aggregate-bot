7a6c7d74e1601694adacea81a4abaf5e219af2ba
rh0danthe <rita.pichushkina.04@mail.ru>
2024-03-25 03:29:38 +0300

    fix: it works

--- nlp/app/nlp_unit.py
+++ nlp/app/nlp_unit.py
@@ -1,4 +1,7 @@
 import natasha
+import pymorphy3
+
+morph = pymorphy3.MorphAnalyzer()
 
 segmenter = natasha.Segmenter()
 morph_vocab = natasha.MorphVocab()
@@ -7,7 +10,6 @@ emb = natasha.NewsEmbedding()
 morph_tagger = natasha.NewsMorphTagger(emb)
 syntax_parser = natasha.NewsSyntaxParser(emb)
 
-
 def form_title(msgs):
     ms = []
     for msg in msgs.msgs:
@@ -15,29 +17,20 @@ def form_title(msgs):
         doc.segment(segmenter)
         doc.tag_morph(morph_tagger)
         doc.parse_syntax(syntax_parser)
+
+
         for token in doc.sents[0].syntax.tokens:
+            #print(token.text, token.rel)
 
-            if token.rel == "nsubj:pass":
-                msg.title = 'Утеряно: ' + token.text
-        ms.append({'chat_id': msg.chat_id,
-                   'message_id': msg.message_id,
-                   'content': msg.content,
-                   'title': msg.title})
-    dd = {'is_found': msgs.is_found,
-          'session': msgs.session,
-          'keywords': msgs.keywords,
-          'msgs': ms}
-    print(type(dd))
-    return dd
-
-
-'''doc = natasha.Doc('Потеряна Галина')
-
-doc.segment(segmenter)
-doc.tag_morph(morph_tagger)
-doc.parse_syntax(syntax_parser)
-print(doc.sents[0].syntax)
-for token in doc.sents[0].syntax.tokens:
-  #print(token)
-  if token.rel == 'nsubj:pass':
-    print('Что потеряно:', token.text)'''
+            if token.rel in ["nsubj:pass", "nsubj", "obj"] :
+                if morph.parse(token.text)[0].normal_form in msgs.keywords:
+                    msg.title = morph.parse(token.text)[0].normal_form
+                    ms.append({'chat_id': msg.chat_id,
+                               'message_id': msg.message_id,
+                               'content': msg.content,
+                               'title': msg.title})
+        print (msg)
+    return {'is_found': msgs.is_found,
+            'session': msgs.session,
+            'keywords': msgs.keywords,
+            'msgs': ms}
